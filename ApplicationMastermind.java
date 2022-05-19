fffe2dd968831af40487e4b45520def374db46ad
Titouan Leteneur <105874474+Titouan-Leteneur@users.noreply.github.com>
2022-05-19 18:16:17 +0200

    Update ApplicationMastermind.java

--- ApplicationMastermind.java
+++ ApplicationMastermind.java
@@ -5,12 +5,12 @@ import javax.swing.JFrame;
 public class ApplicationMastermind {
 	
 	public static void main(String[] args) {
-		// Création de la fenêtre
+		// CrÃ©ation de la fenÃªtre
 		JFrame f = new JFrame();
 		f.setTitle("Mastermind");
 		f.setLayout(new GridLayout(1,1));
 		
-		// Ajout de la vue morpion
+		// Ajout de la vue
 		f.add(new VueMastermind(10, 5));
 		
 		f.pack();
